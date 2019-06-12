5084d1af968d443f958141ac0012e00eb3ea1efc
Imad Kawas <imadakawas@gmail.com>
2019-06-12 15:13:07 +0300

    adding project

---
+++ helloworld/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+import {MovieProvider} from  './MovieContext';
+
+function App() {
+
+ 
+
+
+  return (
+    <MovieProvider>
+    <div className="App">
+     <Nav />
+     <AddMovie/>
+     <MovieList />
+    </div>
+    </MovieProvider>
+  );
+}
+
+export default App;
